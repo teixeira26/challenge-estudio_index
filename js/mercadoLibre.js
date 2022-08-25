13e4a5b2dcf47ee2535d20656636fb2b5a7b8d90
shift <teixeira.matheus2626@gmail.com>
2022-08-25 09:29:16 -0300

    agregado slider sobre fotos de producto

--- js/mercadoLibre.js
+++ js/mercadoLibre.js
@@ -1,8 +1,8 @@
 const urls=['https://api.mercadolibre.com/items?ids=MLA863712882', 'https://api.mercadolibre.com/items/MLA646715969']
-urls.map((url)=>{
+urls.map((url, y)=>{
     fetch(url).then(result=>result.json()).then(async(result)=>{
     let body = (result[0]&& result[0].body)||result
-    let photo = body.pictures[0]
+    let photo = body.pictures
     let price = body.price;
     let originalPrice = body.base_price === price?'':body.base_price
     let infoSeller = await fetch(`https://api.mercadolibre.com/users/${body.seller_id}/shipping_options?zip_code=1754&dimensions=16x16x16,1500`).then(response=>response.json())
@@ -18,7 +18,15 @@ urls.map((url)=>{
     let full = body.shipping.logistic_type==='fulfillment'&&body.status!=='paused'?'Full':'Publicación pausada'
     $('.productos').append(`
     <div class='producto'>  
-        <img src='${photo.url} alt='producto1' class='productPhoto'>
+        <div class = 'productsPhotoContainer${y}'>
+        ${photo.map((x)=>{
+            return `
+            <div class= 'product photo'>
+                <img src=${x.url} alt='producto1' class='productPhoto'>
+            </div>
+            `
+        })}
+        </div>
         <hr>
         <p class='originalPrice'>${originalPrice}</p>
         <p class='price'>$ ${price}</p>
@@ -28,7 +36,9 @@ urls.map((url)=>{
         <h2 class='productTitle'>${body.title}</h2>
         <hr>
     </>
-        `)    
+        `)   
+    urls.forEach((x,y)=>$(`.productsPhotoContainer${y}`).slick())
+    a=$('.productsPhotoContainer')[0]
     console.log(result)})
 
 });
